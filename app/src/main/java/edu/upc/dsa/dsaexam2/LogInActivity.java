9800ec61498c4ed5182cb19a03ba88aba60ca649
MikelArinaMarcos <mikel.arina@estudiantat.upc.edu>
2021-01-11 11:37:07 +0100

    Configuración de la actividad logIn

--- app/src/main/java/edu/upc/dsa/dsaexam2/LogInActivity.java
+++ app/src/main/java/edu/upc/dsa/dsaexam2/LogInActivity.java
@@ -2,13 +2,29 @@ package edu.upc.dsa.dsaexam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
 
 public class LogInActivity extends AppCompatActivity {
+    EditText user;
+    String useName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        user = findViewById((R.id.userText));
+        loadSharedPreference();
+    }
+
+    private void loadSharedPreference() {
+        SharedPreferences preferences = getSharedPreferences("Usuario", Context.MODE_PRIVATE);
+        useName = preferences.getString("userName", "error");
+        Intent intent = new Intent(this, userActivity.class);
     }
 }
